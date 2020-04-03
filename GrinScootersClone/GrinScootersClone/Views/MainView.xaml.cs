6d4e14b4d1f645c4e8b740339b1addeb75c2ed8f
Erick Augusto <erick.dev.it@gmail.com>
2020-04-02 23:26:28 -0300

    Criando as primeiras paginas, e adicionando a tabbed page

---
+++ GrinScootersClone/GrinScootersClone/Views/MainView.xaml.cs
@@ -0,0 +1,29 @@
+﻿using GrinScootersClone.Views.Profile;
+using GrinScootersClone.Views.Trip;
+using GrinScootersClone.Views.Wallet;
+using System.ComponentModel;
+using Xamarin.Forms.PlatformConfiguration;
+using Xamarin.Forms.PlatformConfiguration.AndroidSpecific;
+using TabbedPage = Xamarin.Forms.TabbedPage;
+
+namespace GrinScootersClone.Views
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainView : TabbedPage
+    {
+        public MainView()
+        {
+            InitializeComponent();
+
+            On<Android>().SetToolbarPlacement(ToolbarPlacement.Bottom);
+            On<Android>().DisableSmoothScroll();
+            On<Android>().DisableSwipePaging();
+
+            Children.Add(new TripMapView());
+            Children.Add(new WalletView());
+            Children.Add(new ProfileView());
+        }
+    }
+}
