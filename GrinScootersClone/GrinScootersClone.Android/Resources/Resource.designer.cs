11d79c5f87a22504f451f8b7beab8ec0cc48d84e
Erick Augusto <erick.dev.it@gmail.com>
2020-04-03 01:26:51 -0300

    Adicionando view models

--- GrinScootersClone/GrinScootersClone.Android/Resources/Resource.designer.cs
+++ GrinScootersClone/GrinScootersClone.Android/Resources/Resource.designer.cs
@@ -8352,10 +8352,19 @@ namespace GrinScootersClone.Droid
 			public const int notify_panel_notification_icon_bg = 2131165299;
 			
 			// aapt resource value: 0x7F070074
-			public const int tooltip_frame_dark = 2131165300;
+			public const int tab_profile = 2131165300;
 			
 			// aapt resource value: 0x7F070075
-			public const int tooltip_frame_light = 2131165301;
+			public const int tab_trip = 2131165301;
+			
+			// aapt resource value: 0x7F070076
+			public const int tab_wallet = 2131165302;
+			
+			// aapt resource value: 0x7F070077
+			public const int tooltip_frame_dark = 2131165303;
+			
+			// aapt resource value: 0x7F070078
+			public const int tooltip_frame_light = 2131165304;
 			
 			static Drawable()
 			{
---
+++ GrinScootersClone/GrinScootersClone/ViewModels/ProfileViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GrinScootersClone.ViewModels
+{
+    public class ProfileViewModel
+    {
+    }
+}
