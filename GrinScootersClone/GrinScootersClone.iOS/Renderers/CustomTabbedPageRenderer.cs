80b38682a6618c5b39ba03fde8fd82a3216e51b9
Erick Augusto <erick.dev.it@gmail.com>
2020-04-05 22:54:19 -0300

    Implementando renderer da tabbed page do iOS

---
+++ GrinScootersClone/GrinScootersClone.iOS/Renderers/CustomTabbedPageRenderer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CoreGraphics;
+using Foundation;
+using GrinScootersClone.iOS.Renderers;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(TabbedPage), typeof(CustomTabbedPageRenderer))]
+namespace GrinScootersClone.iOS.Renderers
+{
+    public class CustomTabbedPageRenderer : TabbedRenderer
+    {
+        private readonly float _tabBarHeight = 120f;
+        public override void ViewDidLayoutSubviews()
+        {
+            base.ViewDidLayoutSubviews();
+
+            TabBar.Frame = new CGRect(
+                TabBar.Frame.X,
+                TabBar.Frame.Y + (TabBar.Frame.Height - _tabBarHeight),
+                TabBar.Frame.Width,
+                _tabBarHeight
+            );
+        }
+    }
+}
