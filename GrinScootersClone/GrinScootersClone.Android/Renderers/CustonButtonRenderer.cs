454ee876893f69cc71d56803105e66f649082f2b
Erick Augusto <erick.dev.it@gmail.com>
2020-04-06 23:29:08 -0300

    Workaround para por sombra no botao do scan

---
+++ GrinScootersClone/GrinScootersClone.Android/Renderers/CustonButtonRenderer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using GrinScootersClone.Droid.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using ButtonRenderer = Xamarin.Forms.Platform.Android.FastRenderers.ButtonRenderer;
+
+[assembly: ExportRenderer(typeof(Xamarin.Forms.Button), typeof(CustonButtonRenderer))]
+namespace GrinScootersClone.Droid.Renderers
+{
+    public class CustonButtonRenderer: ButtonRenderer
+    {
+        public CustonButtonRenderer(Context context) 
+            : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.Button> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                Control.SetAllCaps(false);
+            }
+        }
+    }
+}
