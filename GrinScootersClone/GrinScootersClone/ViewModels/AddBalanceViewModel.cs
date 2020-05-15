1471545458cc748c8492c56c64f8c95ce8c3a9ca
Erick Augusto <erick.dev.it@gmail.com>
2020-05-15 00:34:24 -0300

    Implementando a view de adicionar saldo

---
+++ GrinScootersClone/GrinScootersClone/ViewModels/AddBalanceViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace GrinScootersClone.ViewModels
+{
+    public class AddBalanceViewModel : BaseViewModel
+    {
+        #region Fields
+
+        private readonly INavigation _navigation;
+
+        #endregion
+
+        #region Commands
+
+        public Command GoBackCommand { get; private set; }
+
+        #endregion
+
+        #region Constructors
+
+        public AddBalanceViewModel(INavigation navigation)
+        {
+            _navigation = navigation;
+
+            GoBackCommand = new Command(
+                async () => await NavigateToBackAsync()
+            );
+
+            LoadNavBar();
+        }
+
+        #endregion
+
+        #region Methods
+
+        private void LoadNavBar()
+        {
+            Title = "Minha Carteira";
+            NavArrow = "navbar_arrow_black";
+            NavBackgroundColor = "#F5F5F5";
+            NavBarButtonVisible = true;
+        }
+
+        private async Task NavigateToBackAsync()
+        {
+            await _navigation.PopModalAsync();
+        }
+
+        #endregion
+    }
+}
--- GrinScootersClone/GrinScootersClone/Views/Wallet/AddBalanceView.xaml.cs
+++ GrinScootersClone/GrinScootersClone/Views/Wallet/AddBalanceView.xaml.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using GrinScootersClone.ViewModels;
+using System;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -12,9 +9,106 @@ namespace GrinScootersClone.Views.Wallet
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class AddBalanceView : ContentPage
     {
+        private Frame _defaultSelected;
+        private bool _isFirstAppear = true;
+
         public AddBalanceView()
         {
             InitializeComponent();
+            BindingContext = new AddBalanceViewModel(Navigation);
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            if (_isFirstAppear)
+                SelectFirst();
+
+            _isFirstAppear = false;
+        }
+
+        private void SelectFirst()
+        {
+            int index = 0;
+            foreach (var view in GridSection.Children)
+            {
+                if (view is Frame frame)
+                {
+                    if (index++ == 1)
+                    {
+                        GoToStateSelected(frame);
+                        _defaultSelected = frame;
+                        continue;
+                    }
+
+                    GoToStateUnSelected(frame);
+                }
+            }
+
+            GoToStatePopSelected();
+        }
+
+        private void OnTapped(object sender, EventArgs eventArgs)
+        {
+            if (sender is Frame senderFrame)
+            {
+                foreach (var view in GridSection.Children)
+                {
+                    if (view is Frame frame)
+                    {
+                        if (senderFrame == frame)
+                        {
+                            GoToStateSelected(frame);
+                            continue;
+                        }
+
+                        GoToStateUnSelected(frame);
+                    }
+                }
+
+                if (senderFrame == _defaultSelected)
+                    GoToStatePopSelected();
+                else
+                    GoToStatePopUnSelected();
+            }
+        }
+
+        private void GoToStateSelected(Frame frame)
+        {
+            VisualStateManager.GoToState(frame, "Selected");
+
+            if (frame.Children[0] is Label label)
+            {
+                SetBalance(label.Text);
+                VisualStateManager.GoToState(label, "Selected");
+            }
+        }
+
+        private void GoToStateUnSelected(Frame frame)
+        {
+            VisualStateManager.GoToState(frame, "UnSelected");
+
+            if (frame.Children[0] is Label label)
+            {
+                VisualStateManager.GoToState(label, "UnSelected");
+            }
+        }
+
+        private void GoToStatePopSelected()
+        {
+            VisualStateManager.GoToState(PopularFrame, "Selected");
+        }
+
+        private void GoToStatePopUnSelected()
+        {
+            VisualStateManager.GoToState(PopularFrame, "UnSelected");
+        }
+
+        private void SetBalance(string value)
+        {
+            PurchaseBtn.Text = $"Confirme a compra {value}";
+            RechargeSpan.Text = value;
         }
     }
 }
